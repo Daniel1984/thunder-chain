6d30c22687557e8aeb7c1e5404c8ce772ca2b022
Daniel1984 <aciddaniel@gmail.com>
2024-12-30 10:12:31 +0100

    cleanup

---
+++ cmd/blocks/block.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+
+	"com.perkunas/internal/models/block"
+	"com.perkunas/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type App struct {
+	proto.UnimplementedTransactionServiceServer
+	log        *slog.Logger
+	blockModel block.BlockModel
+	apiPort    string
+}
+
+func (app *App) ensureGenesisBlock(ctx context.Context) error {
+	hasGenesis, err := app.blockModel.HasGenesisBlock(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to check for genesis block presence %w", err)
+	}
+
+	if !hasGenesis {
+		// create genesis block
+		var block block.BlockDB
+		if err := json.Unmarshal([]byte(genesisJson), &block); err != nil {
+			return fmt.Errorf("unable to unmarshal genesis block json %w", err)
+		}
+
+		blockHash, err := block.CalculateHash()
+		if err != nil {
+			return fmt.Errorf("unable to calculate genesis block hash %w", err)
+		}
+
+		block.Hash = blockHash
+		if err := app.blockModel.Save(ctx, block); err != nil {
+			return fmt.Errorf("unable to persist genesis block %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (app *App) CreateBlock(ctx context.Context, in *proto.CreateBlockRequest) (*proto.CreateBlockResponse, error) {
+	txsJson, err := json.Marshal(in.Block.Transactions)
+	if err != nil {
+		app.log.Info("failed to Marshal in.Block.Transactions", "err", err)
+		return nil, status.Error(codes.Internal, "failed processing request payload")
+	}
+
+	pld := block.BlockDB{
+		Block: block.Block{
+			Hash:       in.Block.Hash,
+			PrevHash:   in.Block.PrevHash,
+			MerkleRoot: in.Block.MerkleRoot,
+			Timestamp:  in.Block.Timestamp,
+			Height:     in.Block.Height,
+			Nonce:      in.Block.Nonce,
+		},
+		TransactionsDB: txsJson,
+	}
+
+	if err := app.blockModel.Save(ctx, pld); err != nil {
+		app.log.Info("failed persisting block data", "err", err, "pld", pld)
+		return nil, status.Error(codes.Internal, "failed persisting block")
+	}
+
+	return &proto.CreateBlockResponse{Hash: pld.Hash, Height: pld.Height}, nil
+}
--- cmd/blocks/main.go
+++ cmd/blocks/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	_ "embed"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -13,9 +12,6 @@ import (
 	"com.perkunas/internal/logger"
 	"com.perkunas/internal/models/block"
 	"com.perkunas/internal/sqlite"
-	"com.perkunas/proto"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 //go:embed sql/blocks.sql
@@ -23,13 +19,6 @@ var blocksSql string
 
 //go:embed genesis.json
 var genesisJson string
-
-type App struct {
-	proto.UnimplementedTransactionServiceServer
-	log        *slog.Logger
-	blockModel block.BlockModel
-	apiPort    string
-}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -58,60 +47,6 @@ func main() {
 		app.log.Error("failed to start grpc server", "err", err)
 		os.Exit(1)
 	}
-}
-
-func (app *App) ensureGenesisBlock(ctx context.Context) error {
-	hasGenesis, err := app.blockModel.HasGenesisBlock(ctx)
-	if err != nil {
-		return fmt.Errorf("unable to check for genesis block presence %w", err)
-	}
-
-	if !hasGenesis {
-		// create genesis block
-		var block block.BlockDB
-		if err := json.Unmarshal([]byte(genesisJson), &block); err != nil {
-			return fmt.Errorf("unable to unmarshal genesis block json %w", err)
-		}
-
-		blockHash, err := block.CalculateHash()
-		if err != nil {
-			return fmt.Errorf("unable to calculate genesis block hash %w", err)
-		}
-
-		block.Hash = blockHash
-		if err := app.blockModel.Save(ctx, block); err != nil {
-			return fmt.Errorf("unable to persist genesis block %w", err)
-		}
-	}
-
-	return nil
-}
-
-func (app *App) CreateBlock(ctx context.Context, in *proto.CreateBlockRequest) (*proto.CreateBlockResponse, error) {
-	txsJson, err := json.Marshal(in.Block.Transactions)
-	if err != nil {
-		app.log.Info("failed to Marshal in.Block.Transactions", "err", err)
-		return nil, status.Error(codes.Internal, "failed processing request payload")
-	}
-
-	pld := block.BlockDB{
-		Block: block.Block{
-			Hash:       in.Block.Hash,
-			PrevHash:   in.Block.PrevHash,
-			MerkleRoot: in.Block.MerkleRoot,
-			Timestamp:  in.Block.Timestamp,
-			Height:     in.Block.Height,
-			Nonce:      in.Block.Nonce,
-		},
-		TransactionsDB: txsJson,
-	}
-
-	if err := app.blockModel.Save(ctx, pld); err != nil {
-		app.log.Info("failed persisting block data", "err", err, "pld", pld)
-		return nil, status.Error(codes.Internal, "failed persisting block")
-	}
-
-	return &proto.CreateBlockResponse{Hash: pld.Hash, Height: pld.Height}, nil
 }
 
 func dbConnect(ctx context.Context, dbName, sql string) (*sqlite.DB, error) {
