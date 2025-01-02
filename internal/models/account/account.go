2f149f4a06a0d4f0e0a076ea5c0108d81e814e55
Daniel1984 <aciddaniel@gmail.com>
2025-01-02 10:54:41 +0100

    updating accounts model

--- internal/models/account/account.go
+++ internal/models/account/account.go
@@ -1,9 +1,21 @@
 package account
 
+import "com.perkunas/proto"
+
 type Account struct {
 	ID        int64  `json:"id" db:"id"`
 	Address   string `json:"address" db:"address"`
 	Balance   int64  `json:"balance" db:"balance"`
 	Nonce     uint64 `json:"nonce" db:"nonce"`
 	Timestamp int64  `json:"timestamp" db:"timestamp"`
+}
+
+func (acc Account) ToProto() *proto.Account {
+	return &proto.Account{
+		Id:        acc.ID,
+		Address:   acc.Address,
+		Balance:   acc.Balance,
+		Nonce:     acc.Nonce,
+		Timestamp: acc.Timestamp,
+	}
 }
