2340187f24c1a11987e36f4020a8abdcb6994557
Daniel1984 <aciddaniel@gmail.com>
2025-01-02 10:04:57 +0100

    adding rpc clients to miner

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -1,36 +1,86 @@
 package main
 
 import (
-	"context"
+	"flag"
 	"log/slog"
-	"time"
+	"os"
 
 	"com.perkunas/internal/logger"
-	"com.perkunas/internal/models/block"
 	"com.perkunas/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type App struct {
-	log        *slog.Logger
-	mempoolAPI string
-	blockModel block.Model
-	rpcClient  proto.TransactionServiceClient
+	log         *slog.Logger
+	mempoolAPI  string
+	stateAPI    string
+	blocksAPI   string
+	accountsAPI string
+	mempoolRPC  proto.MempoolServiceClient
+	stateRPC    proto.StateChangeServiceClient
+	blocksRPC   proto.BlockServiceClient
 }
 
 func main() {
-	_, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	app := &App{log: logger.WithJSONFormat().With(slog.String("scope", "miner-svc"))}
+	flag.StringVar(&app.mempoolAPI, "mempoolapi", os.Getenv("MEMPOOL_API"), "mempool api endpoint")
+	flag.StringVar(&app.stateAPI, "stateapi", os.Getenv("STATE_API"), "state api endpoint")
 
-	log := logger.WithJSONFormat().With(slog.String("scope", "node"))
-
-	// blocksDB, err := dbConnection(ctx, "blocks.db", blocksSql)
-	// if err != nil {
-	// 	log.Error("failed connecting to db", "err", err)
-	// 	os.Exit(1)
-	// }
-	// defer blocksDB.Close()
-	for {
-		time.Sleep(10 * time.Second)
-		log.Info("slept for 10s")
+	// initiate mempool rpc client
+	mempoolConn, mempoolClient, err := mempoolRPCClient(app.mempoolAPI)
+	if err != nil {
+		app.log.Error("mempool grpc did not connect", "err", err)
+		os.Exit(1)
 	}
+	defer mempoolConn.Close()
+	app.mempoolRPC = mempoolClient
+
+	// initiate state rpc client
+	stateConn, stateClient, err := stateRPCClient(app.stateAPI)
+	if err != nil {
+		app.log.Error("state grpc did not connect", "err", err)
+		os.Exit(1)
+	}
+	defer stateConn.Close()
+	app.stateRPC = stateClient
+
+	// initiate blocks rpc client
+	blocksConn, blocksClient, err := blocksRPCClient(app.blocksAPI)
+	if err != nil {
+		app.log.Error("blocks grpc did not connect", "err", err)
+		os.Exit(1)
+	}
+	defer blocksConn.Close()
+	app.blocksRPC = blocksClient
+}
+
+func mempoolRPCClient(apiUrl string) (*grpc.ClientConn, proto.MempoolServiceClient, error) {
+	conn, err := grpc.NewClient(apiUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := proto.NewMempoolServiceClient(conn)
+	return conn, client, nil
+}
+
+func stateRPCClient(apiUrl string) (*grpc.ClientConn, proto.StateChangeServiceClient, error) {
+	conn, err := grpc.NewClient(apiUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := proto.NewStateChangeServiceClient(conn)
+	return conn, client, nil
+}
+
+func blocksRPCClient(apiUrl string) (*grpc.ClientConn, proto.BlockServiceClient, error) {
+	conn, err := grpc.NewClient(apiUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := proto.NewBlockServiceClient(conn)
+	return conn, client, nil
 }
